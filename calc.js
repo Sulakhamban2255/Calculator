a9ad50be2f683ad49ed513af2deca99b113a1af3
sulakh1993 <sulakh281293@gmail.com>
2024-05-29 14:26:30 +0530

    Calculator

---
+++ calc.js
@@ -0,0 +1,13 @@
+const displayData = (n) =>{
+result.value+=n
+}
+function findOut(){
+    result.value = eval(result.value)
+}
+const allClear=()=>{
+
+result.value = ""
+}
+const backSpace=()=>{
+    result.value = result.value.slice(0, -1);
+}
